3b69f2d83b48cdccccccdd8fef2c2c08f6a74efd
kimmg19 <115523825+kimmg19@users.noreply.github.com>
2024-01-25 11:06:54 +0900

    3d 모델링 연습
    캐릭터 공격 모션 구현하기

--- Assets/Scripts/TPSCharacterController.cs
+++ Assets/Scripts/TPSCharacterController.cs
@@ -17,18 +17,28 @@ public class TPSCharacterController : MonoBehaviour {
     void Update() {
         LookAround();
         Move();
+        Attack();
     }
     void Move() {
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         bool isMove = moveInput.magnitude != 0;
         animator.SetBool("isMove", isMove);
-        if(isMove ) {
+        if(isMove) {
             Vector3 lookForward=new Vector3(cameraArm.forward.x, 0f,cameraArm.forward.z).normalized;
             Vector3 lookRight=new Vector3(cameraArm.right.x,0f,cameraArm.right.z).normalized;
             Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
             characterBody.forward = moveDir;
             transform.position += moveDir * Time.deltaTime * 8f;
         }
+    }
+
+    void Attack() {
+        //if (!animator.GetCurrentAnimatorStateInfo(0).IsName("isAttack")) {
+        //    bool isAttack = Input.GetMouseButtonDown(0);
+        //    animator.SetBool("isAttack", isAttack);
+        //}
+        bool isAttack = Input.GetMouseButtonDown(0);
+        animator.SetBool("isAttack", isAttack);
     }
 
     void LookAround() {
